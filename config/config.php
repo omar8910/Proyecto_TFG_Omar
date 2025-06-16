7d59ea09b48ea0de588484de2a1f42cc90614acc
Omar Qneiby <omarqneiby@gmail.com>
2025-06-16 20:26:21 +0200

    Actualizada URL para hosting, y corregido el tamaño de fuente de la página

--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
 <?php
-const BASE_URL = 'http://localhost/proyectoOmarPHPFinal/';
+// const BASE_URL = 'http://localhost/proyectoOmarPHPFinal/';
+// const BASE_URL = 'https://0f91-2a0c-5a82-270c-8000-d88a-f895-448a-d225.ngrok-free.app/proyectoOmarPHPFinal/';
+const BASE_URL = 'https://crisp-truly-buck.ngrok-free.app/proyectoOmarPHPFinal/';
 
 ?>
--- src/Views/Carrito/verCarrito.php
+++ src/Views/Carrito/verCarrito.php
@@ -110,6 +110,8 @@
     }
 
     .formatContainer {
+        margin-top: 15px;
+        margin-bottom: 15px;
         display: flex;
         justify-content: center;
         align-items: center;
